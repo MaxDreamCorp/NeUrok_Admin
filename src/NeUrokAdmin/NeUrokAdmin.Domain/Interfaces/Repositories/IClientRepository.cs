79fa2f73f9a26777ac9ded62810712f11a13cfa4
MadMaxGames <maxmalk220@gmail.com>
2026-04-24 16:41:53 +0300

    Implemented client repository

---
+++ src/NeUrokAdmin/NeUrokAdmin.Domain/Interfaces/Repositories/IClientRepository.cs
@@ -0,0 +1,14 @@
+﻿using NeUrokAdmin.Domain.Entities;
+
+namespace NeUrokAdmin.Domain.Interfaces.Repositories
+{
+    public interface IClientRepository
+    {
+        Task AddAsync(Client client, CancellationToken cancellationToken = default);
+        Task RemoveAsync(Client client, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Client client, CancellationToken cancellationToken = default);
+        Task<int> GetNextIdAsync(CancellationToken cancellationToken = default);
+        Task<Client?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<List<Client>> GetAllAsync(CancellationToken cancellationToken = default);
+    }
+}
