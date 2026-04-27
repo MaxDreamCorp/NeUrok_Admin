3e9a076d38fd6765adb5dcd54cf0a1ad988da22a
MadMaxGames <maxmalk220@gmail.com>
2026-04-27 14:47:25 +0300

    Implemented client status repository

---
+++ src/NeUrokAdmin/NeUrokAdmin.Domain/Interfaces/Repositories/IClientStatusRepository.cs
@@ -0,0 +1,11 @@
+﻿using NeUrokAdmin.Domain.Entities;
+
+namespace NeUrokAdmin.Domain.Interfaces.Repositories
+{
+    public interface IClientStatusRepository
+    {
+        Task<ClientStatus?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<ClientStatus?> GetByNameAsync(string status, CancellationToken cancellationToken = default);
+        Task<List<ClientStatus>> GetAllAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/NeUrokAdmin/NeUrokAdmin.Infrastructure/Persistance/Repositories/ClientStatusRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NeUrokAdmin.Domain.Entities;
+using NeUrokAdmin.Domain.Interfaces.Repositories;
+
+namespace NeUrokAdmin.Infrastructure.Persistance.Repositories
+{
+    public class ClientStatusRepository : IClientStatusRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientStatusRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ClientStatus>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.ClientStatuses.ToListAsync(cancellationToken);
+        }
+
+        public async Task<ClientStatus?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _context.ClientStatuses.FindAsync(id, cancellationToken);
+        }
+
+        public async Task<ClientStatus?> GetByNameAsync(string status, CancellationToken cancellationToken = default)
+        {
+            return await _context.ClientStatuses.FirstOrDefaultAsync(cs => cs.Status == status, cancellationToken);
+        }
+    }
+}
