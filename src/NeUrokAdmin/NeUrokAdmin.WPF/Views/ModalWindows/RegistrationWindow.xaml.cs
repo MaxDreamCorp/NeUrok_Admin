c2cb67a0b10ed72227d51ff0fc2077ba3af29921
MadMaxGames <maxmalk220@gmail.com>
2026-04-28 10:14:15 +0300

    Changed registration, removed migration entities

--- src/NeUrokAdmin/NeUrokAdmin.WPF/Views/ModalWindows/RegistrationWindow.xaml.cs
+++ src/NeUrokAdmin/NeUrokAdmin.WPF/Views/ModalWindows/RegistrationWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using MediatR;
+using NeUrokAdmin.Application.Features.Authorization.Commands;
+using NeUrokAdmin.WPF.Interfaces;
 using NeUrokAdmin.WPF.Services;
 
 namespace NeUrokAdmin.WPF.Views.ModalWindows
@@ -10,12 +13,15 @@ namespace NeUrokAdmin.WPF.Views.ModalWindows
     public partial class RegistrationWindow : Window
     {
         private readonly NavigationService _navigationService;
+        private readonly IDialogService _dialogService;
+        private readonly IMediator _mediator;
 
-        public RegistrationWindow(NavigationService navigationService)
+        public RegistrationWindow(NavigationService navigationService, IDialogService dialogService, IMediator mediator)
         {
             InitializeComponent();
             _navigationService = navigationService;
-
+            _dialogService = dialogService;
+            _mediator = mediator;
         }
 
         private void PassInp_PasswordChanged(object sender, RoutedEventArgs e)
@@ -54,6 +60,36 @@ namespace NeUrokAdmin.WPF.Views.ModalWindows
                     RegistrateBtn.IsEnabled = true;
                 }
             }
+        }
+
+        private async void RegistrateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginInp.Text) || string.IsNullOrEmpty(PassInp.Password))
+            {
+                _dialogService.ShowWarning("Не все поля заполнены");
+                return;
+            }
+
+            if (!_dialogService.AskQuetion($"Вы уверены, что хотите зарегистрировать пользователя {LoginInp.Text}?"))
+                return;
+
+            var cmd = new RegistrateCommand(LoginInp.Text, PassInp.Password);
+
+            try
+            {
+                await _mediator.Send(cmd);
+                _dialogService.ShowMessage($"Пользователь {LoginInp.Text} успешно зарегистрирован", "Успех");
+                Close();
+            }
+            catch (Exception ex)
+            {
+                _dialogService.ShowError(ex.Message, "Ошибка входа");
+            }
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
