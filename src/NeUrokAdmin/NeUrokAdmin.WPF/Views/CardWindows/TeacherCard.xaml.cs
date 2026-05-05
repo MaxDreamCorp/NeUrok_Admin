ab30299c1ef4a53b95210b58614540cf88cdeca7
MadMaxGames <maxmalk220@gmail.com>
2026-05-05 13:41:41 +0300

    Created teacher card

---
+++ src/NeUrokAdmin/NeUrokAdmin.WPF/Views/CardWindows/TeacherCard.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using NeUrokAdmin.WPF.Views.ViewModels.Cards;
+
+namespace NeUrokAdmin.WPF.Views.CardWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для TeacherCard.xaml
+    /// </summary>
+    public partial class TeacherCard : Window
+    {
+        public TeacherCardViewModel ViewModel { get; set; } = null!;
+
+        public TeacherCard()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = ViewModel;
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DelBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AcceptBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
