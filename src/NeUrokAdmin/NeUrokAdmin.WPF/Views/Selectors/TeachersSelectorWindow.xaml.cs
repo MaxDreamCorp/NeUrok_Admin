0fb29c84fbd17ba0a8fe0eca7b6dfe3778b85605
MadMaxGames <maxmalk220@gmail.com>
2026-05-05 12:54:06 +0300

    Implemented selection logic in teacher selector

---
+++ src/NeUrokAdmin/NeUrokAdmin.WPF/Views/Selectors/TeachersSelectorWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MediatR;
+using NeUrokAdmin.Application.Features.TeacherOperations.Queries;
+using NeUrokAdmin.Domain.DTOs;
+using NeUrokAdmin.WPF.Views.ViewModels.Selectors;
+using NeUrokAdmin.WPF.Views.ViewModels.Selectors.SelectorItems;
+
+namespace NeUrokAdmin.WPF.Views.Selectors
+{
+    /// <summary>
+    /// Логика взаимодействия для TeachersSelectorWindow.xaml
+    /// </summary>
+    public partial class TeachersSelectorWindow : Window
+    {
+        public TeacherSelectorViewModel ViewModel { get; set; } = null!;
+        public event EventHandler<List<TeacherDTO>>? TeachersSelected;
+
+        private readonly IMediator _mediator;
+
+        public TeachersSelectorWindow(IMediator mediator)
+        {
+            InitializeComponent();
+            _mediator = mediator;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = ViewModel;
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void QuickSearchInp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CopyFilterSelectedToAllTeachers();
+
+            TeachersSelected?.Invoke(this, ViewModel.AllTeachers.Where(ci => ci.IsSelected)
+                .Select(ci => ci.Teacher).ToList());
+            Close();
+        }
+
+        private void ListBoxItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (!ViewModel.IsSingleton)
+                return;
+
+            if (sender is CheckBox checkBox && checkBox.DataContext is TeacherSelectorItemViewModel vm)
+            {
+                var it = ViewModel.AllTeachers.Find(ci => ci.Teacher.Id == vm.Teacher.Id);
+                if (it != null)
+                {
+                    foreach (var teacher in ViewModel.AllTeachers)
+                    {
+                        if (teacher.Teacher.Id != it.Teacher.Id)
+                            teacher.IsSelected = false;
+                    }
+                }
+            }
+        }
+
+        private async Task RefreeshList()
+        {
+            var selectedTeachers = ViewModel.AllTeachers.Where(ci => ci.IsSelected).Select(ci => ci.Teacher).ToList();
+
+            var allTeachers = await _mediator.Send(new GetAllTeachersQuery());
+            var newVM = new TeacherSelectorViewModel(allTeachers, selectedTeachers);
+            ViewModel = newVM;
+            DataContext = ViewModel;
+        }
+
+        private void CopyFilterSelectedToAllTeachers()
+        {
+            foreach (var selectionItem in ViewModel.FilteredTeachers)
+            {
+                if (selectionItem.IsSelected)
+                {
+                    var it = ViewModel.AllTeachers.Find(ci => ci.Teacher.Id == selectionItem.Teacher.Id);
+                    if (it != null && !it.IsSelected)
+                        it.IsSelected = true;
+                }
+            }
+        }
+
+    }
+}
---
+++ src/NeUrokAdmin/NeUrokAdmin.WPF/Views/ViewModels/Selectors/TeacherSelectorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using NeUrokAdmin.Domain.DTOs;
+using NeUrokAdmin.WPF.Views.ViewModels.Selectors.SelectorItems;
+
+namespace NeUrokAdmin.WPF.Views.ViewModels.Selectors
+{
+    public partial class TeacherSelectorViewModel : ObservableObject
+    {
+        public List<TeacherSelectorItemViewModel> AllTeachers { get; set; }
+
+        [ObservableProperty]
+        private List<TeacherSelectorItemViewModel> _filteredTeachers;
+
+        public bool IsSingleton { get; set; }
+
+        public TeacherSelectorViewModel(List<TeacherDTO> allTeachers, List<TeacherDTO> selectedTeachers)
+        {
+            var selectedIds = selectedTeachers.Select(c => c.Id).ToList();
+            AllTeachers = new(allTeachers.Select(c => new TeacherSelectorItemViewModel(c, selectedIds.Contains(c.Id))));
+            _filteredTeachers = AllTeachers;
+        }
+
+        public TeacherSelectorViewModel(List<TeacherDTO> allTeachers, TeacherDTO? selectedTeacher)
+        {
+            AllTeachers = new(allTeachers.Select(c => new TeacherSelectorItemViewModel(c, selectedTeacher != null && c.Id == selectedTeacher.Id)));
+            _filteredTeachers = AllTeachers;
+            IsSingleton = true;
+        }
+    }
+}
