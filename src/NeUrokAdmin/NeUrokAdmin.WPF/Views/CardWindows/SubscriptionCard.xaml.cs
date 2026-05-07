bcff516cc45653243c0bc6da55b509e4ed12e6f7
MadMaxGames <maxmalk220@gmail.com>
2026-05-07 16:46:59 +0300

    Created subscription card design & vm

---
+++ src/NeUrokAdmin/NeUrokAdmin.WPF/Views/CardWindows/SubscriptionCard.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using MediatR;
+using NeUrokAdmin.WPF.Interfaces;
+using NeUrokAdmin.WPF.Services;
+using NeUrokAdmin.WPF.Views.ViewModels.Cards;
+
+namespace NeUrokAdmin.WPF.Views.CardWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для SubscriptionCard.xaml
+    /// </summary>
+    public partial class SubscriptionCard : Window
+    {
+        public SubscriptionCardViewModel ViewModel { get; set; } = null!;
+
+        private readonly IMediator _mediator;
+        private readonly NavigationService _navigationService;
+        private readonly IDialogService _dialogService;
+
+        public SubscriptionCard(NavigationService navigationService, IMediator mediator, IDialogService dialogService)
+        {
+            InitializeComponent();
+            _navigationService = navigationService;
+            _mediator = mediator;
+            _dialogService = dialogService;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = ViewModel;
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DelBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AcceptBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/NeUrokAdmin/NeUrokAdmin.WPF/Views/ViewModels/Cards/SubscriptionCardViewModel.cs
@@ -0,0 +1,104 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using NeUrokAdmin.Domain.DTOs;
+using NeUrokAdmin.WPF.Enums;
+
+namespace NeUrokAdmin.WPF.Views.ViewModels.Cards
+{
+    public partial class SubscriptionCardViewModel : ObservableObject
+    {
+        public OperationType OperationType
+        {
+            get => _operationType;
+            set
+            {
+                SetProperty(ref _operationType, value);
+
+                switch (value)
+                {
+                    case OperationType.Create:
+                        IsFilter = false;
+                        IsEditable = true;
+                        IsDeletable = false;
+                        HeaderText = "Создание абонемента";
+                        break;
+                    case OperationType.Read:
+                        IsFilter = false;
+                        IsEditable = false;
+                        IsDeletable = false;
+                        HeaderText = $"Абонемент \"{Name}\"";
+                        break;
+                    case OperationType.Edit:
+                        IsFilter = false;
+                        IsEditable = true;
+                        IsDeletable = true;
+                        HeaderText = $"Абонемент \"{Name}\"";
+                        break;
+                    case OperationType.Filter:
+                        IsFilter = true;
+                        IsEditable = true;
+                        IsDeletable = false;
+                        HeaderText = "Фильтр абонементов";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private OperationType _operationType;
+
+        [ObservableProperty]
+        private List<string> _classesTypes = new();
+
+        public List<ClassesTypeDTO> ClassesTypesDTO
+        {
+            get => _classesTypesDTO;
+            set
+            {
+                SetProperty(ref _classesTypesDTO, value);
+                ClassesTypes = value.Select(cs => cs.Type).ToList();
+            }
+        }
+        private List<ClassesTypeDTO> _classesTypesDTO = new();
+
+        [ObservableProperty]
+        private bool _isDeletable;
+
+        [ObservableProperty]
+        private bool _isEditable;
+
+        [ObservableProperty]
+        private bool _isFilter;
+
+        [ObservableProperty]
+        private string _headerText = null!;
+
+        [ObservableProperty]
+        private int? _id;
+
+        [ObservableProperty]
+        private string _name = string.Empty;
+
+        [ObservableProperty]
+        private string _classesType = string.Empty;
+
+        [ObservableProperty]
+        private decimal? _cost;
+
+        [ObservableProperty]
+        private int? _classesAmount;
+
+        public SubscriptionCardViewModel(OperationType operationType, SubscriptionDTO? subscriptionDTO = null)
+        {
+            if (subscriptionDTO != null)
+            {
+                _id = subscriptionDTO.Id;
+                _name = subscriptionDTO.Name;
+                _classesType = subscriptionDTO.ClassesType.Type;
+                _cost = subscriptionDTO.Cost;
+                _classesAmount = subscriptionDTO.ClassesAmount;
+            }
+            OperationType = operationType;
+        }
+    }
+}
