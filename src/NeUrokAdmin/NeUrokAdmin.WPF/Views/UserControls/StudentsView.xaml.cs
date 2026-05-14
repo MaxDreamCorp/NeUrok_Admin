ec4e939041926fe97d9efcdecc1f53b734b65540
MadMaxGames <maxmalk220@gmail.com>
2026-05-14 12:06:39 +0300

    Added quick search feature for students

--- src/NeUrokAdmin/NeUrokAdmin.WPF/Views/UserControls/StudentsView.xaml.cs
+++ src/NeUrokAdmin/NeUrokAdmin.WPF/Views/UserControls/StudentsView.xaml.cs
@@ -42,12 +42,13 @@ namespace NeUrokAdmin.WPF.Views.UserControls
             if (card.DialogResult == true)
             {
                 await PrintAll();
+                QuickSearch();
             }
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            QuickSearch();
         }
 
         private void FilterBtn_Click(object sender, RoutedEventArgs e)
@@ -55,9 +56,10 @@ namespace NeUrokAdmin.WPF.Views.UserControls
 
         }
 
-        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        private async void ClearBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            ViewModel.QuickSearchText = string.Empty;
+            await PrintAll();
         }
 
         private async void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -72,7 +74,7 @@ namespace NeUrokAdmin.WPF.Views.UserControls
                 {
                     await PrintAll();
                     //await Refilter();
-                    //QuickSearch();
+                    QuickSearch();
                 }
             }
         }
@@ -82,6 +84,36 @@ namespace NeUrokAdmin.WPF.Views.UserControls
             var qry = new GetAllStudentsQuery();
             ViewModel.AllStudents = await _mediator.Send(qry);
             ViewModel.DisplayedStudents = new(ViewModel.AllStudents);
+        }
+
+        private void ResetDisplayedStudentsAfterSearching()
+        {
+            if (ViewModel.FilteredStudents == null)
+                ViewModel.DisplayedStudents = new(ViewModel.AllStudents);
+            else
+                ViewModel.DisplayedStudents = new(ViewModel.FilteredStudents);
+        }
+
+        private void QuickSearch()
+        {
+            var searchText = ViewModel.QuickSearchText.ToLower();
+            if (string.IsNullOrEmpty(searchText))
+            {
+                ResetDisplayedStudentsAfterSearching();
+                return;
+            }
+
+            var initialList = ViewModel.FilteredStudents?.ToList() ?? ViewModel.AllStudents;
+
+            ViewModel.DisplayedStudents = new(initialList.Where(s =>
+                s.Id.ToString().Contains(searchText) ||
+                s.Client.ChildFullname.ToLower().Contains(searchText) ||
+                s.StudentSubscriptions.Any(ss => ss.Subscription.Name.ToLower().Contains(searchText)) ||
+                s.StudentSubscriptions.Any(ss => ss.Course.Name.ToLower().Contains(searchText)) ||
+                s.StudentSubscriptions.Any(ss => ss.SubscriptionStatus.Status.ToLower().Contains(searchText)) ||
+                s.StudentSubscriptions.Any(ss => ss.StartDate.ToDateTime(TimeOnly.MinValue).ToString("dd.MM.yyyy").Contains(searchText)) ||
+                s.StudentSubscriptions.Any(ss => ss.FinishDate.ToDateTime(TimeOnly.MinValue).ToString("dd.MM.yyyy").Contains(searchText))
+                ));
         }
     }
 }
